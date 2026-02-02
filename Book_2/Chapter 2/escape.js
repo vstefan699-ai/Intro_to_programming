fb7e56fe43bd884dedc92c8fdad138f383288846
vstefan699-ai <vstefan699@gmail.com>
2026-02-02 17:41:05 +0200

    Add files via upload

---
+++ Book_2/Chapter 2/escape.js
@@ -0,0 +1,11 @@
+//Example 1
+let text = "We are the so-called \"Vikings\" from the north.";
+console.log(text);
+
+//Example 2
+let text2 = 'It\'s Alright.';
+console.log(text2);
+
+//Example 3
+let text3 = 'It\'s Cool.';
+console.log(text3, '\n', 'oky');
---
+++ Book_2/Chapter 2/syntax.js
@@ -0,0 +1,26 @@
+alert("Let's Have Fun With JavaScript!");
+
+for (var i = 0; i < 10; i++) {
+  document.write("Coding is fun!<br>");
+}
+
+for (var i = 0; i < 5; i++) {
+  document.write("Joe said, \"Hi!\"<br>");
+}
+
+for (var i = 0; i < 3; i++) {
+  document.write("First JavaScript Code<br>");
+}
+
+
+
+/* 
+-----------multi comment line----------
+
+Write some text directly to the HTML output by using document.write().
+document.write() still works perfectly in all major browsers for now, but it's deprecated, meaning it's discouraged and may be removed in future versions.
+
+ \" is an escape character.It tells JavaScript to treat the double quote as a character, not the end of the string.
+ 
+*/
+
